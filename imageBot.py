32e0c5cbe5bea868c7f26933a0b15a2a73b04af1
Nikolas Larson <76064681+NLarson2504@users.noreply.github.com>
2023-02-02 14:31:56 -0500

    Update imageBot.py
    Formatting

--- imageBot.py
+++ imageBot.py
@@ -23,14 +23,10 @@ def main():
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    ########################################################################
-    # Let us show some of the training images, for fun.
-
     import matplotlib.pyplot as plt
     import numpy as np
 
     # functions to show an image
-
 
     def imshow(img):
         img = img / 2 + 0.5     # unnormalize
@@ -49,9 +45,7 @@ def main():
     print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
 
 
-    ########################################################################
     # 2. Define a Convolutional Neural Network
-    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     # Copy the neural network from the Neural Networks section before and modify it to
     # take 3-channel images (instead of 1-channel images as it was defined).
 
@@ -81,22 +75,16 @@ def main():
 
     net = Net()
 
-    ########################################################################
     # 3. Define a Loss function and optimizer
-    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # Let's use a Classification Cross-Entropy loss and SGD with momentum.
+    # use a Classification Cross-Entropy loss and SGD with momentum.
 
     import torch.optim as optim
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    ########################################################################
     # 4. Train the network
-    # ^^^^^^^^^^^^^^^^^^^^
-    #
-    # This is when things start to get interesting.
-    # We simply have to loop over our data iterator, and feed the inputs to the
+    # simply loop over the data iterator, and feed the inputs to the
     # network and optimize.
 
     for epoch in range(3):  # loop over the dataset multiple times
@@ -123,27 +111,12 @@ def main():
 
     print('Finished Training')
 
-    ########################################################################
-    # Let's quickly save our trained model:
+    # quickly save the trained model:
 
     PATH = './cifar_net.pth'
     torch.save(net.state_dict(), PATH)
 
-    ########################################################################
-    # See `here <https://pytorch.org/docs/stable/notes/serialization.html>`_
-    # for more details on saving PyTorch models.
-    #
     # 5. Test the network on the test data
-    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    #
-    # We have trained the network for 2 passes over the training dataset.
-    # But we need to check if the network has learnt anything at all.
-    #
-    # We will check this by predicting the class label that the neural network
-    # outputs, and checking it against the ground-truth. If the prediction is
-    # correct, we add the sample to the list of correct predictions.
-    #
-    # Okay, first step. Let us display an image from the test set to get familiar.
 
     dataiter = iter(testloader)
     images, labels = next(dataiter)
@@ -152,61 +125,43 @@ def main():
     imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
 
-    ########################################################################
-    # Next, let's load back in our saved model (note: saving and re-loading the model
-    # wasn't necessary here, we only did it to illustrate how to do so):
-
     net = Net()
     net.load_state_dict(torch.load(PATH))
-
-    ########################################################################
+    
     # Okay, now let us see what the neural network thinks these examples above are:
 
     outputs = net(images)
 
-    ########################################################################
     # The outputs are energies for the 10 classes.
     # The higher the energy for a class, the more the network
     # thinks that the image is of the particular class.
-    # So, let's get the index of the highest energy:
+ 
     _, predicted = torch.max(outputs, 1)
 
     print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
                                   for j in range(4)))
 
-    ########################################################################
-    # The results seem pretty good.
-    #
-    # Let us look at how the network performs on the whole dataset.
 
     correct = 0
     total = 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
+    # since it's not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in testloader:
             images, labels = data
             # calculate outputs by running images through the network
             outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
+            # the class with the highest energy is what is choosen as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
-
-    ########################################################################
-    # That looks way better than chance, which is 10% accuracy (randomly picking
-    # a class out of 10 classes).
-    # Seems like the network learnt something.
-    #
-    # Hmmm, what are the classes that performed well, and the classes that did
-    # not perform well:
-
+    
     # prepare to count predictions for each class
     correct_pred = {classname: 0 for classname in classes}
     total_pred = {classname: 0 for classname in classes}
 
-    # again no gradients needed
+    # no gradients needed
     with torch.no_grad():
         for data in testloader:
             images, labels = data
